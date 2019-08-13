741b5fd36bc6a432765b9f5e8c4e733b7fcd55ce
Richard Bennett <richardbennett2008@gmail.com>
2019-08-13 08:40:24 +0100

    bug fixes to headers

--- mail.go
+++ mail.go
@@ -5,11 +5,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
+	"time"
 )
 
 type Mail interface {
-	GetMailMesasges(string) ([]MailMessage, error)
-	GetInboxMailFromAddress(string) ([]MailMessage, error)
+	GetMailMesasges(string) (MailMessage, error)
+	GetInboxMailFromAddress(string) (MailMessage, error)
 	GetMessageAttachement() (MessageAttachment, error)
 }
 
@@ -19,58 +21,58 @@ type MailRequest struct {
 }
 
 type MailMessage struct {
-	_odata_context  string `json:"@odata.context"`
-	_odata_nextLink string `json:"@odata.nextLink"`
-	Message         []struct {
-		_odata_etag   string        `json:"@odata.etag"`
-		BccRecipients []interface{} `json:"bccRecipients"`
-		Body          struct {
-			Content     string `json:"content"`
-			ContentType string `json:"contentType"`
-		} `json:"body"`
-		BodyPreview     string        `json:"bodyPreview"`
-		Categories      []interface{} `json:"categories"`
-		CcRecipients    []interface{} `json:"ccRecipients"`
-		ChangeKey       string        `json:"changeKey"`
-		ConversationID  string        `json:"conversationId"`
-		CreatedDateTime string        `json:"createdDateTime"`
-		Flag            struct {
-			FlagStatus string `json:"flagStatus"`
-		} `json:"flag"`
-		From struct {
-			EmailAddress struct {
-				Address string `json:"address"`
-				Name    string `json:"name"`
-			} `json:"emailAddress"`
-		} `json:"from"`
-		HasAttachments             bool          `json:"hasAttachments"`
+	OdataContext  string `json:"@odata.context"`
+	OdataNextLink string `json:"@odata.nextLink"`
+	Value         []struct {
+		OdataEtag                  string        `json:"@odata.etag"`
 		ID                         string        `json:"id"`
-		Importance                 string        `json:"importance"`
-		InferenceClassification    string        `json:"inferenceClassification"`
+		CreatedDateTime            time.Time     `json:"createdDateTime"`
+		LastModifiedDateTime       time.Time     `json:"lastModifiedDateTime"`
+		ChangeKey                  string        `json:"changeKey"`
+		Categories                 []interface{} `json:"categories"`
+		ReceivedDateTime           time.Time     `json:"receivedDateTime"`
+		SentDateTime               time.Time     `json:"sentDateTime"`
+		HasAttachments             bool          `json:"hasAttachments"`
 		InternetMessageID          string        `json:"internetMessageId"`
-		IsDeliveryReceiptRequested interface{}   `json:"isDeliveryReceiptRequested"`
-		IsDraft                    bool          `json:"isDraft"`
-		IsRead                     bool          `json:"isRead"`
-		IsReadReceiptRequested     bool          `json:"isReadReceiptRequested"`
-		LastModifiedDateTime       string        `json:"lastModifiedDateTime"`
+		Subject                    string        `json:"subject"`
+		BodyPreview                string        `json:"bodyPreview"`
+		Importance                 string        `json:"importance"`
 		ParentFolderID             string        `json:"parentFolderId"`
-		ReceivedDateTime           string        `json:"receivedDateTime"`
-		ReplyTo                    []interface{} `json:"replyTo"`
-		Sender                     struct {
+		ConversationID             string        `json:"conversationId"`
+		IsDeliveryReceiptRequested interface{}   `json:"isDeliveryReceiptRequested"`
+		IsReadReceiptRequested     bool          `json:"isReadReceiptRequested"`
+		IsRead                     bool          `json:"isRead"`
+		IsDraft                    bool          `json:"isDraft"`
+		WebLink                    string        `json:"webLink"`
+		InferenceClassification    string        `json:"inferenceClassification"`
+		Body                       struct {
+			ContentType string `json:"contentType"`
+			Content     string `json:"content"`
+		} `json:"body"`
+		Sender struct {
 			EmailAddress struct {
-				Address string `json:"address"`
 				Name    string `json:"name"`
+				Address string `json:"address"`
 			} `json:"emailAddress"`
 		} `json:"sender"`
-		SentDateTime string `json:"sentDateTime"`
-		Subject      string `json:"subject"`
+		From struct {
+			EmailAddress struct {
+				Name    string `json:"name"`
+				Address string `json:"address"`
+			} `json:"emailAddress"`
+		} `json:"from"`
 		ToRecipients []struct {
 			EmailAddress struct {
-				Address string `json:"address"`
 				Name    string `json:"name"`
+				Address string `json:"address"`
 			} `json:"emailAddress"`
 		} `json:"toRecipients"`
-		WebLink string `json:"webLink"`
+		CcRecipients  []interface{} `json:"ccRecipients"`
+		BccRecipients []interface{} `json:"bccRecipients"`
+		ReplyTo       []interface{} `json:"replyTo"`
+		Flag          struct {
+			FlagStatus string `json:"flagStatus"`
+		} `json:"flag"`
 	} `json:"value"`
 }
 
@@ -90,64 +92,60 @@ type MessageAttachment struct {
 	} `json:"value"`
 }
 
-func (request MailRequest) GetInboxMail(bearerToken string) ([]MailMessage, error) {
+func (request MailRequest) GetInboxMail(bearerToken string) (MailMessage, error) {
 	url := "https://graph.microsoft.com/v1.0/me/messages"
 
 	req, _ := http.NewRequest("GET", url, nil)
 
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", request.BearerAccessToken))
-	req.Header.Add("Accept", "*/*")
-	req.Header.Add("Cache-Control", "no-cache")
-	req.Header.Add("Host", "graph.microsoft.com")
-	req.Header.Add("Accept-Encoding", "gzip, deflate")
-	req.Header.Add("Connection", "keep-alive")
 	req.Header.Add("cache-control", "no-cache")
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return []MailMessage{}, err
+		return MailMessage{}, err
 	}
 
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return []MailMessage{}, err
+		return MailMessage{}, err
 	}
 
-	var messages []MailMessage
+	var messages MailMessage
 	err = json.Unmarshal(body, &messages)
 
 	return messages, nil
 }
 
-func (request MailRequest) GetInboxMailFromAddress(fromAddress string) ([]MailMessage, error) {
-	url := fmt.Sprintf("https://graph.microsoft.com/v1.0/me/messages?$filter=(from/emailAddress/address) eq '%s'", fromAddress)
+func (request MailRequest) GetInboxMailFromAddress(fromAddress string) (MailMessage, error) {
+	queryParams := fmt.Sprintf("(from/emailAddress/address) eq '%s'", fromAddress)
+	queryParams = url.QueryEscape(queryParams)
+	queryURL := fmt.Sprintf("https://graph.microsoft.com/v1.0/me/messages?$filter=%s", queryParams)
 
-	req, _ := http.NewRequest("GET", url, nil)
+	req, _ := http.NewRequest("GET", queryURL, nil)
 
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", request.BearerAccessToken))
-	req.Header.Add("Accept", "*/*")
-	req.Header.Add("Cache-Control", "no-cache")
-	req.Header.Add("Host", "graph.microsoft.com")
-	req.Header.Add("Accept-Encoding", "gzip, deflate")
-	req.Header.Add("Connection", "keep-alive")
 	req.Header.Add("cache-control", "no-cache")
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return []MailMessage{}, err
+		return MailMessage{}, err
 	}
 
 	defer res.Body.Close()
 	body, err := ioutil.ReadAll(res.Body)
+
 	if err != nil {
-		return []MailMessage{}, err
+		return MailMessage{}, err
 	}
 
-	var messages []MailMessage
-	err = json.Unmarshal(body, &messages)
+	var messages MailMessage
+	if err := json.Unmarshal(body, &messages); err != nil {
+		fmt.Println(err)
+		return MailMessage{}, err
+	}
 
 	return messages, nil
 }
